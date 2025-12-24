84d48bfdc9a503aa37da71ca6e8f53ce76155227
Dreamboat09 <enechimdindu@gmail.com>
2025-12-24 22:03:14 +0100

    created a generator that streams rows from an SQL database one by one

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+
+def stream_users():
+    
+   # Streams rows from the user_data table one by one using a generator.
+   
+
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="your_password",
+        database="ALX_prodev"
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:  
+        yield row
+
+    cursor.close()
+    connection.close()
